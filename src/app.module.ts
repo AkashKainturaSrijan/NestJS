0fddcecc00098b1b61a2c003e86be17edf7f34f3
akash.kaintura <akash.kaintura@srijan.net>
2022-10-12 15:26:17 +0530

    add: JWT with user

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
 import { FilterModule } from './filter/filter.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [PostsModule, FilterModule],
+  imports: [
+    PostsModule,
+    FilterModule,
+    MongooseModule.forRoot('mongodb://localhost/authentication'),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
