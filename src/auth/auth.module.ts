711007f7b54b5272de884b38cf99c1fe68a9c98f
akash.kaintura <akash.kaintura@srijan.net>
2022-10-12 15:44:33 +0530

    fix: auth module problem

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,25 @@
 import { Module } from '@nestjs/common';
+import { UserModule } from 'src/users/users.module';
+import { AuthService } from './auth.service';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthController } from './auth.controller';
+import { UsersService } from 'src/users/users.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserSchema } from '../users/users.model';
+import { LocalStrategy } from './local.auth';
 
-@Module({})
+@Module({
+  imports: [
+    UserModule,
+    PassportModule,
+    JwtModule.register({
+      secret: 'secretKey',
+      signOptions: { expiresIn: '60s' },
+    }),
+    MongooseModule.forFeature([{ name: 'user', schema: UserSchema }]),
+  ],
+  providers: [AuthService, UsersService, LocalStrategy],
+  controllers: [AuthController],
+})
 export class AuthModule {}
