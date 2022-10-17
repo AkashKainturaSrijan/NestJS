cd52d33109496b052bc6f80761c16acc917557d4
akash.kaintura <akash.kaintura@srijan.net>
2022-10-17 14:33:56 +0530

    fix: api-file decorator

--- src/files/api-file.decorator.ts
+++ src/files/api-file.decorator.ts
@@ -1,16 +1,22 @@
 import { applyDecorators, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { MulterOptions } from '@nestjs/platform-express/multer/interfaces/multer-options.interface';
 import { ApiBody, ApiConsumes } from '@nestjs/swagger';
 
-export function ApiFile() {
+export function ApiFile(
+  fieldName = 'file',
+  required = false,
+  localOptions?: MulterOptions,
+) {
   return applyDecorators(
-    UseInterceptors(FileInterceptor('file')),
+    UseInterceptors(FileInterceptor(fieldName, localOptions)),
     ApiConsumes('multipart/form-data'),
     ApiBody({
       schema: {
         type: 'object',
+        required: required ? [fieldName] : [],
         properties: {
-          file: {
+          [fieldName]: {
             type: 'string',
             format: 'binary',
           },
--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,16 +1,6 @@
-import {
-  Controller,
-  Post,
-  UploadedFile,
-  UploadedFiles,
-  UseInterceptors,
-} from '@nestjs/common';
-import {
-  FileFieldsInterceptor,
-  FileInterceptor,
-  FilesInterceptor,
-} from '@nestjs/platform-express';
-import { ApiBody, ApiConsumes, ApiTags } from '@nestjs/swagger';
+import { Controller, Post, UploadedFile } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { ApiFile } from './api-file.decorator';
 import { FilesService } from './files.service';
 
 @Controller('files')
@@ -19,70 +9,8 @@ export class FilesController {
   constructor(private readonly filesService: FilesService) {}
 
   @Post('upload')
-  @UseInterceptors(FileInterceptor('file'))
-  @ApiConsumes('multipart/form-data')
-  @ApiBody({
-    schema: {
-      type: 'object',
-      properties: {
-        file: {
-          type: 'string',
-          format: 'binary',
-        },
-      },
-    },
-  })
+  @ApiFile('avatar', true) // 🤩 changing field name and set file required
   uploadFile(@UploadedFile() file: Express.Multer.File) {
     console.log(file);
-  }
-
-  @Post('uploads')
-  @UseInterceptors(FilesInterceptor('files')) // 👈  using FilesInterceptor here
-  @ApiConsumes('multipart/form-data')
-  @ApiBody({
-    schema: {
-      type: 'object',
-      properties: {
-        files: {
-          type: 'array', // 👈  array of files
-          items: {
-            type: 'string',
-            format: 'binary',
-          },
-        },
-      },
-    },
-  })
-  uploadFiles(@UploadedFiles() files: Array<Express.Multer.File>) {
-    console.log(files);
-  }
-
-  @Post('uploadFields')
-  @UseInterceptors(
-    FileFieldsInterceptor([
-      // 👈  multiple files with different field names
-      { name: 'avatar', maxCount: 1 },
-      { name: 'background', maxCount: 1 },
-    ]),
-  )
-  @ApiConsumes('multipart/form-data')
-  @ApiBody({
-    schema: {
-      type: 'object',
-      properties: {
-        // 👈  field names need to be repeated for swagger
-        avatar: {
-          type: 'string',
-          format: 'binary',
-        },
-        background: {
-          type: 'string',
-          format: 'binary',
-        },
-      },
-    },
-  })
-  uploadMultipleFiles(@UploadedFiles() files: Express.Multer.File[]) {
-    console.log(files);
   }
 }
